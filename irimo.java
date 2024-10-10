f2523864cbcaa5efe5e26ddbdfbbf75697f252da
jreleaserbot <jreleaser@kordamp.org>
2024-10-09 20:51:33 -0500

    irimo v0.0.1

---
+++ irimo.java
@@ -0,0 +1,10 @@
+//usr/bin/env jbang "$0" "$@" ; exit $?
+//JAVA 17+
+//DEPS com.garodriguezlp.irimo:irimo:0.0.1
+// Generated with JReleaser 1.14.0 at 2024-10-09T20:51:13.742655-05:00
+
+public class irimo {
+    public static void main(String... args) throws Exception {
+        org.springframework.boot.loader.launch.JarLauncher.main(args);
+    }
+}
