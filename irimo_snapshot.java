b629fb48242ee59eac0d37f24d9cd6b78b69959d
jreleaserbot <jreleaser@kordamp.org>
2024-10-09 19:56:11 -0500

    irimo early-access

--- irimo_snapshot.java
+++ irimo_snapshot.java
@@ -2,7 +2,7 @@
 //JAVA 17+
 //REPOS jitpack
 //DEPS com.github.garodriguezlp:irimo:main-SNAPSHOT
-// Generated with JReleaser 1.14.0 at 2024-10-09T14:17:44.341947-05:00
+// Generated with JReleaser 1.14.0 at 2024-10-09T19:56:08.642977-05:00
 
 public class irimo_snapshot {
     public static void main(String... args) throws Exception {
