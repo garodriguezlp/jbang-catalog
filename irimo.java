62ead37fb3721bb6570d8de46f615f4f84f9e8f6
jreleaserbot <jreleaser@kordamp.org>
2025-02-15 20:57:41 -0500

    irimo v0.0.2

--- irimo.java
+++ irimo.java
@@ -2,7 +2,7 @@
 //JAVA 17+
 //REPOS jitpack
 //DEPS com.github.garodriguezlp:irimo:v0.0.2
-// Generated with JReleaser 1.16.0 at 2025-01-18T16:33:59.42721023-05:00
+// Generated with JReleaser 1.16.0 at 2025-02-15T20:57:38.913608365-05:00
 
 public class irimo {
     public static void main(String... args) throws Exception {
