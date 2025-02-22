1da80a8ab4e06164d3ae14657d0f6c85447def89
jreleaserbot <jreleaser@kordamp.org>
2025-02-22 01:32:49 -0500

    irimo early-access

--- irimo_snapshot.java
+++ irimo_snapshot.java
@@ -1,8 +1,8 @@
 //usr/bin/env jbang "$0" "$@" ; exit $?
-//JAVA 17+
+//JAVA 17
 //REPOS jitpack
 //DEPS com.github.garodriguezlp:irimo:main-SNAPSHOT
-// Generated with JReleaser 1.16.0 at 2025-02-22T01:17:16.438076764-05:00
+// Generated with JReleaser 1.16.0 at 2025-02-22T01:32:47.340762711-05:00
 
 public class irimo_snapshot {
     public static void main(String... args) throws Exception {
